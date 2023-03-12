e37189c83fcc10e613e0d3112d504e79a906d20c
John Mayou <johnm.ceo@gmail.com>
2023-03-12 18:56:03 -0500

    mutiple dates picker imported and added to join cohort page

--- src/pages/User/JoinCohort/components/JoinCohortPage/JoinCohortPage.jsx
+++ src/pages/User/JoinCohort/components/JoinCohortPage/JoinCohortPage.jsx
@@ -1,17 +1,26 @@
 import "./JoinCohortPage.scss";
 import Sidebar from "../../../../../layout/Sidebar/Sidebar";
+import MultipleDatesPicker from "@ambiot/material-ui-multiple-dates-picker";
+import Button from "@mui/material/Button";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 function JoinCohortPage() {
 	const user = useSelector((store) => store.user);
+	const [open, setOpen] = useState(false);
 
 	return (
 		<div className="join-cohort">
 			<Sidebar />
 			<section className="join-cohort__main-content">
 				<h1>JOIN COHORT</h1>
-				<h2>Welcome, {user.email}!</h2>
-				<p>Your ID is: {user.id}</p>
+				<Button onClick={() => setOpen(!open)}>Selected Dates</Button>
+				<MultipleDatesPicker
+					open={open}
+					selectedDates={[]}
+					onCancel={() => setOpen(false)}
+					onSubmit={(dates) => console.log("selected dates", dates)}
+				/>
 			</section>
 		</div>
 	);
