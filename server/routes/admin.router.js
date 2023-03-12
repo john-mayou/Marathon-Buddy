6bb7a593636b8a5c1d287f25cf805c4fba27eed0
John Mayou <johnm.ceo@gmail.com>
2023-03-11 21:10:47 -0600

    admin.router endpoint now rejects users that are not 'admin's, authenticate admin middle added in middleware file

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
+const {
+	rejectUnauthenticated,
+	rejectIfNotAdmin,
+} = require("../modules/authentication-middleware");
 
 /**
  * GET route template
  */
-router.get("/", (req, res) => {
+router.get("/", rejectUnauthenticated, rejectIfNotAdmin, (req, res) => {
 	res.send("/api/admin working");
 });
 
