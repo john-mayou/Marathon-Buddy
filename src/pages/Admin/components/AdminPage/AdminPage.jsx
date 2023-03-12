e7d268a7234567fe691db2c11e06cd38c6feffa5
John Mayou <johnm.ceo@gmail.com>
2023-03-12 14:59:12 -0500

    empty table added to admin page

--- src/pages/Admin/components/AdminPage/AdminPage.jsx
+++ src/pages/Admin/components/AdminPage/AdminPage.jsx
@@ -3,9 +3,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function AdminPage() {
-	const [adminText, setAdminText] = useState("");
+	const [cohorts, setCohorts] = useState([]);
 
 	useEffect(() => {
+		getCohorts();
+	}, []);
+
+	const getCohorts = () => {
 		axios
 			.get("/api/admin")
 			.then((response) => {
@@ -14,12 +18,33 @@ function AdminPage() {
 			.catch((error) => {
 				console.log(error);
 			});
-	}, []);
+	};
 
 	return (
 		<>
 			<h1>AdminPage</h1>
-			<p>{adminText}</p>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Start Date</th>
+						<th>Users</th>
+						<th>Completion %</th>
+					</tr>
+				</thead>
+				<tbody>
+					{cohorts.map((cohort) => {
+						return (
+							<tr key={cohort.id}>
+								<td>{cohort.name}</td>
+								<td>{cohort.start_date}</td>
+								<td>{cohort.users}</td>
+								<td>{cohort.completion_percentage}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
 		</>
 	);
 }
