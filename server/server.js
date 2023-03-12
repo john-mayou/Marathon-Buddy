6432766b9b561370a88040bbd344ae616cfe6b42
John Mayou <johnm.ceo@gmail.com>
2023-03-11 23:16:41 -0600

    added new router for strava auth, /api/strava-auth. Endpoint works on connect to strava button click

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require("./strategies/user.strategy");
 // Route includes
 const userRouter = require("./routes/user.router");
 const adminRouter = require("./routes/admin.router");
+const stravaRouter = require("./routes/strava.router");
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
+app.use("/api/strava-auth", stravaRouter);
 
 // Serve static files
 app.use(express.static("build"));
--- src/pages/User/Apps/components/AppsPage/AppsPage.jsx
+++ src/pages/User/Apps/components/AppsPage/AppsPage.jsx
@@ -1,15 +1,28 @@
 import "./AppsPage.scss";
 import Sidebar from "../../../../../layout/Sidebar/Sidebar";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 function AppsPage() {
 	const user = useSelector((store) => store.user);
+
+	const handleConnectStrava = () => {
+		axios
+			.get("/api/strava-auth")
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
 	return (
 		<div className="connected-apps">
 			<Sidebar />
 			<section className="connected-apps__main-content">
 				<h1>CONNECTED APPS</h1>
+				<button onClick={handleConnectStrava}>Connect to Strava</button>
 				<h2>Welcome, {user.email}!</h2>
 				<p>Your ID is: {user.id}</p>
 			</section>
