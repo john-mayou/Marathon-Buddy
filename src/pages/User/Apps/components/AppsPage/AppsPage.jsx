7fa4b783f03ff0ed25242c504cc2d1642c4a6e27
John Mayou <johnm.ceo@gmail.com>
2023-03-12 14:28:55 -0500

    button on connected-apps page now redirects to strava authorization page

--- src/pages/User/Apps/components/AppsPage/AppsPage.jsx
+++ src/pages/User/Apps/components/AppsPage/AppsPage.jsx
@@ -6,23 +6,15 @@ import axios from "axios";
 function AppsPage() {
 	const user = useSelector((store) => store.user);
 
-	const handleConnectStrava = () => {
-		axios
-			.get("/api/strava-auth")
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+	// lets user approve scope for API access
+	const stravaAuthUrl = `https://www.strava.com/oauth/authorize?client_id=102249&response_type=code&redirect_uri=http://localhost:5000/api/strava-auth&approval_prompt=force&scope=activity:read_all`;
 
 	return (
 		<div className="connected-apps">
 			<Sidebar />
 			<section className="connected-apps__main-content">
 				<h1>CONNECTED APPS</h1>
-				<button onClick={handleConnectStrava}>Connect to Strava</button>
+				<a href={stravaAuthUrl}>Connect to Strava</a>
 				<h2>Welcome, {user.email}!</h2>
 				<p>Your ID is: {user.id}</p>
 			</section>
