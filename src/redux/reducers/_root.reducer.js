197fd9718af0249afab380465975509daa5ccd9d
John Mayou <johnm.ceo@gmail.com>
2023-03-13 13:00:32 -0500

    changed cohorts saga and reducer to 'admin-cohorts....' to reflect actual information they are providing

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import errors from "./errors.reducer";
 import user from "./user.reducer";
-import adminCohorts from "./cohort.reducer";
+import adminCohorts from "./admin-cohorts.reducer";
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
