4d3bd7992391874ff91186dcaa3aee5e352c81fc
John Mayou <johnm.ceo@gmail.com>
2023-03-18 14:19:15 -0500

    sidebar reworked to open up on hover, now only takes up a very small amount of the screen when not being used

--- src/layout/Sidebar/Sidebar.jsx
+++ src/layout/Sidebar/Sidebar.jsx
@@ -1,36 +1,77 @@
 import "./Sidebar.scss";
 import { useHistory, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 
 function UserTemplate({ PageContent }) {
 	const history = useHistory();
 	const dispatch = useDispatch();
 
+	// Dashboard history.push("/dashboard")
+	// Join Next history.push("/join-info")
+	// History history.push("/cohort-history")
+	// Connected Apps history.push("/connected-apps")
+	// Logout dispatch({ type: "LOGOUT" })
+
 	return (
-		<nav className="sidebar">
-			<div>
-				<span>Cohorts</span>
-				<ul className="sidebar__cohort-list">
-					<li onClick={() => history.push("/dashboard")}>
-						Dashboard
-					</li>
-					<li onClick={() => history.push("/join-info")}>
-						Join Next
-					</li>
-					<li onClick={() => history.push("/cohort-history")}>
-						History
-					</li>
-				</ul>
-			</div>
-			<div>
-				<span>Account</span>
-				<ul className="sidebar__account-list">
-					<li onClick={() => history.push("/connected-apps")}>
-						Connected Apps
-					</li>
-					<li onClick={() => dispatch({ type: "LOGOUT" })}>Logout</li>
-				</ul>
-			</div>
+		<nav className="navbar">
+			<ul className="navbar__nav">
+				<li className="navbar__logo">
+					<a className="navbar__link">
+						<span className="logo-text link-text">Marathon</span>
+						<FontAwesomeIcon
+							icon={faAnglesRight}
+							className="logo-icon link-icon"
+						/>
+					</a>
+				</li>
+				<li
+					className="navbar__item"
+					onClick={() => history.push("/dashboard")}
+				>
+					<a className="navbar__link">
+						<FontAwesomeIcon icon={faHouse} className="link-icon" />
+						<span className="link-text">Dashboard</span>
+					</a>
+				</li>
+				<li
+					className="navbar__item"
+					onClick={() => history.push("/join-info")}
+				>
+					<a className="navbar__link">
+						<FontAwesomeIcon icon={faHouse} className="link-icon" />
+						<span className="link-text">Join</span>
+					</a>
+				</li>
+				<li
+					className="navbar__item"
+					onClick={() => history.push("/cohort-history")}
+				>
+					<a className="navbar__link">
+						<FontAwesomeIcon icon={faHouse} className="link-icon" />
+						<span className="link-text">History</span>
+					</a>
+				</li>
+				<li
+					className="navbar__item"
+					onClick={() => history.push("/connected-apps")}
+				>
+					<a className="navbar__link">
+						<FontAwesomeIcon icon={faHouse} className="link-icon" />
+						<span className="link-text">Connected Apps</span>
+					</a>
+				</li>
+				<li
+					className="navbar__item"
+					onClick={() => dispatch({ type: "LOGOUT" })}
+				>
+					<a className="navbar__link">
+						<FontAwesomeIcon icon={faHouse} className="link-icon" />
+						<span className="link-text">Logout</span>
+					</a>
+				</li>
+			</ul>
 		</nav>
 	);
 }
