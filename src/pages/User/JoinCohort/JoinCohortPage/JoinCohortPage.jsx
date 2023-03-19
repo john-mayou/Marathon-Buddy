4ab1d22098a1ea1b19d0cfda9a6909982a00831d
John Mayou <johnm.ceo@gmail.com>
2023-03-18 19:13:54 -0500

    initial styling for join-cohort page

--- src/pages/User/JoinCohort/JoinCohortPage/JoinCohortPage.jsx
+++ src/pages/User/JoinCohort/JoinCohortPage/JoinCohortPage.jsx
@@ -1,8 +1,11 @@
 import "./JoinCohortPage.scss";
 import Sidebar from "../../../../layout/Sidebar/Sidebar";
 import DatePicker from "react-multi-date-picker";
-import DatePanel from "react-multi-date-picker/plugins/date_panel";
+import "react-multi-date-picker/styles/layouts/mobile.css";
 import Select from "@mui/material/Select";
+import DatePanel from "react-multi-date-picker/plugins/date_panel";
+import Button from "@mui/material/Button";
+import Slider from "@mui/material/Slider";
 import MenuItem from "@mui/material/MenuItem";
 import Header from "../../../../components/Header/Header";
 import { useSelector, useDispatch } from "react-redux";
@@ -21,6 +24,7 @@ function JoinCohortPage() {
 	// local state
 	const [trainingDates, setTrainingDates] = useState([]); // for calendar state
 	const [trainingMiles, setTrainingMiles] = useState({});
+	const [stake, setStake] = useState(20);
 
 	// variables
 	const { trainingDuration } = useParams(); // from url (string)
@@ -42,110 +46,159 @@ function JoinCohortPage() {
 	return (
 		<div>
 			<Sidebar />
-			<section className="join-cohort-main">
-				<Header text={"Dashboard"} />
-				<DatePicker
-					multiple
-					value={trainingDates}
-					onChange={(e) => {
-						setTrainingDates(e);
-						setTrainingMiles(
-							e.reduce((obj, date) => {
-								obj[date] = 0;
-								return obj;
-							}, {})
-						);
-					}}
-					format="YYYY-MM-DD"
-					plugins={[<DatePanel />]}
-					render={(value, openCalendar) => {
-						return (
-							<button onClick={openCalendar}>Select Dates</button>
-						);
-					}}
-					minDate={availableDates.min}
-					maxDate={availableDates.max}
-				/>
-				{<p>{currentCohort?.name}</p>}
-				{
-					<p>
-						Start Date:{" "}
-						{dayjs(currentCohort?.start_date).format("MMM D")}
-					</p>
-				}
-				<p>Duration: 7 days</p>
-				<table>
-					<thead>
-						<tr>
-							<th>Date</th>
-							<th>Miles</th>
-						</tr>
-					</thead>
-					<tbody>
-						{trainingDates.map((day, i) => {
-							return (
-								<tr key={i}>
-									<td>
-										{dayjs(new Date(day)).format("MMM D")}
-									</td>
-									<td>
-										<Select
-											label={"Miles"}
-											value={
-												trainingMiles[
-													dayjs(new Date(day)).format(
-														"YYYY-MM-DD"
-													)
-												]
-											}
-											onChange={(e) => {
-												const newTrainingMiles = {
-													...trainingMiles,
-												};
-												newTrainingMiles[day] =
-													e.target.value;
-												setTrainingMiles(
-													newTrainingMiles
-												);
-											}}
-										>
-											{[...Array(26).keys()].map(
-												(notUsed, i) => {
-													return (
-														<MenuItem
-															key={i}
-															value={i}
-														>
-															{i}
-														</MenuItem>
-													);
-												}
-											)}
-										</Select>
-									</td>
+			<main className="join-cohort-main">
+				<Header text={`Join Cohort`} />
+				<section className="join-form">
+					<section className="join-form__info-container">
+						<p>The {currentCohort?.name} Cohort!</p>
+						<p>
+							Start Date:{" "}
+							<strong>
+								{dayjs(currentCohort?.start_date).format(
+									"MMM D"
+								)}
+							</strong>
+						</p>
+						<p>
+							Duration: <strong>{trainingDuration} days</strong>
+						</p>
+					</section>
+					<section className="join-form__stake-container">
+						<span>
+							How much would you want to stake?{" "}
+							<strong>${stake}</strong>
+						</span>
+						<Slider
+							className="stake-input-slider"
+							aria-label="Stake"
+							onChange={(e) => setStake(e.target.value)}
+							value={stake}
+							valueLabelDisplay="auto"
+							step={5}
+							marks
+							min={5}
+							max={25}
+						/>
+					</section>
+					<section className="join-form__date-input-container">
+						<DatePicker
+							className="rmdp-mobile"
+							style={{ padding: "1rem" }}
+							multiple
+							value={trainingDates}
+							onChange={(e) => {
+								setTrainingDates(e);
+								setTrainingMiles(
+									e.reduce((obj, date) => {
+										obj[date] = 0;
+										return obj;
+									}, {})
+								);
+							}}
+							format="YYYY-MM-DD"
+							plugins={[<DatePanel />]}
+							render={(value, openCalendar) => {
+								return (
+									<button
+										className="date-picker-button"
+										onClick={openCalendar}
+									>
+										Select Dates
+									</button>
+								);
+							}}
+							minDate={availableDates.min}
+							maxDate={availableDates.max}
+						/>
+					</section>
+					<section className="join-form__table-container">
+						<table>
+							<thead>
+								<tr>
+									<th>Date</th>
+									<th>Miles</th>
 								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-				<button
-					onClick={() => {
-						axios
-							.post("/api/stripe/create-checkout-session", {
-								dates: JSON.stringify(trainingMiles),
-								cohort_id: currentCohort.id,
-							})
-							.then((response) => {
-								window.location = response.data.url;
-							})
-							.catch((error) => {
-								console.log(error);
-							});
-					}}
-				>
-					Submit
-				</button>
-			</section>
+							</thead>
+							<tbody>
+								{trainingDates.map((day, i) => {
+									return (
+										<tr key={i}>
+											<td>
+												{dayjs(new Date(day)).format(
+													"dddd MMM D"
+												)}
+											</td>
+											<td>
+												<Select
+													label={"Miles"}
+													value={
+														trainingMiles[
+															dayjs(
+																new Date(day)
+															).format(
+																"YYYY-MM-DD"
+															)
+														]
+													}
+													onChange={(e) => {
+														const newTrainingMiles =
+															{
+																...trainingMiles,
+															};
+														newTrainingMiles[day] =
+															e.target.value;
+														setTrainingMiles(
+															newTrainingMiles
+														);
+													}}
+												>
+													{[...Array(26).keys()].map(
+														(notUsed, i) => {
+															return (
+																<MenuItem
+																	key={i}
+																	value={i}
+																>
+																	{i}
+																</MenuItem>
+															);
+														}
+													)}
+												</Select>
+											</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</table>
+					</section>
+					<section className="join-form__checkout-container">
+						<Button
+							variant="contained"
+							onClick={() => {
+								axios
+									.post(
+										"/api/stripe/create-checkout-session",
+										{
+											dates: JSON.stringify(
+												trainingMiles
+											),
+											cohort_id: currentCohort.id,
+										}
+									)
+									.then((response) => {
+										window.location = response.data.url;
+									})
+									.catch((error) => {
+										console.log(error);
+									});
+							}}
+						>
+							Checkout
+						</Button>
+					</section>
+				</section>
+			</main>
 		</div>
 	);
 }
